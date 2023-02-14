b977296ba69dfdbe52c5f3ac95b9979dc9244765
Ivan Martinez <201720533@tese.edu.mx>
2023-02-14 12:09:35 -0600

    importando react-router-dom para poder agregar rutas al proyecto

--- cacei-front/src/App.jsx
+++ cacei-front/src/App.jsx
@@ -1,11 +1,13 @@
-import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import './App.css';
+import Login from './components/Login';
 
 function App() {
-  
-
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path='/' element = {<Login />}/>
+      </Routes>
     </div>
   )
 }
