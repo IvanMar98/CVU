65541a33b52caced59f66e8c1263670f5e60d132
Ivan Martinez <201720533@tese.edu.mx>
2023-02-14 13:06:13 -0600

    agregando componente Header

--- cacei-front/src/App.jsx
+++ cacei-front/src/App.jsx
@@ -2,10 +2,12 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css';
 import Login from './components/Login';
 import Inicio from './components/Inicio';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route path='/' element = {<Login />}/>
         <Route path='/inicio' element = {<Inicio />}></Route>
